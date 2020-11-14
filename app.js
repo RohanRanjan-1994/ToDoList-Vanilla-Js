62bd67787d371371bfd96789db3ef691f0610b8b
Rohan Ranjan <rohanranjanghosh@gmail.com>
2020-11-14 09:44:19 +0530

    Fourth commit: Initial prototype

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function addTodo(event){
     todoDiv.classList.add("todo");
     // Create li tag
     const newTodo = document.createElement("li");
-    newTodo.innerText = "hey";
+    newTodo.innerText = todoInput.value;
     newTodo.classList.add("todo-item");
     todoDiv.appendChild(newTodo);
     // Check Mark Button
@@ -27,8 +27,10 @@ function addTodo(event){
     // Check Trash Button
     const trashButton = document.createElement("button");
     trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-    trashButton.classList.add("complete-btn");
+    trashButton.classList.add("trash-btn");
     todoDiv.appendChild(trashButton);
     // Append to List
     todoList.appendChild(todoDiv);
+    // Clear Todo Input Value
+    todoInput.value = "";
 }
